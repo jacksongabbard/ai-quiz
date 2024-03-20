081dc71718b72d8a6b9a2dbc1b374e75779df99c
Josh Watzman <jwatzman@cord.com>
2024-03-20 16:36:34 +0000

    Add more questions

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -16,11 +16,31 @@ export const questions: BaseQuizQuestion[] = [
 preferences. The people in the first group individually believe they make up a
 majority. What is likely to be true of the second group?`,
     answers: [
-      `They believe they are a minority`,
-      `They believe the groups are equal`,
-      `They believe themselves to be a majority.`,
-      `They have no consistent beliefs`,
+      'They believe they are a minority',
+      'They believe the groups are equal',
+      'They believe themselves to be a majority',
+      'They have no consistent beliefs',
     ],
     correctAnswerIndex: 2,
+  },
+  {
+    question:
+      'Consider the sequence 3, 7, 31, 127, 8191. Which of these numbers comes next?',
+    answers: ['9001', '16383', '131071', '262104'],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: `Adam's net worth is $100. He gives half of that to Beth. She
+gives half of that to Charles. He adds $1 of his own and gives half of that
+to Denise. She gives all of that to Adam. Who of the following is the
+wealthiest person?`,
+    answers: ['Adam', 'Taylor Swift', 'King Charles III', 'Elon Musk'],
+    correctAnswerIndex: 3,
+  },
+  {
+    question:
+      'Sally sat on the seashore slurping a slushie. In what style should she sing after she swallows her snack?',
+    answers: ['Loudly', 'Softly', 'Angrily', 'Lazily'],
+    correctAnswerIndex: 1,
   },
 ];
