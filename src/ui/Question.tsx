dc3be38cfd678310b29da86a44e23c5658793876
Jackson Gabbard <jg@jg.gg>
2024-03-20 14:19:31 +0000

    Some rudamentary responsiveness

--- src/ui/Question.tsx
+++ src/ui/Question.tsx
@@ -21,56 +21,82 @@ export default function Question({
   onSubmit: (humanAnswer: number, botAnswer: number) => void;
 }) {
   const [_humanAnswer, setHumanAnswer] = useState(humanAnswer);
-  const [_botAnswer, setBotAnswer] = useState(botAnswer);
+  const [_botAnswer, setBotAnswer] = useState(botAnswer || 0);
 
   let runningTotal = qq.question.length + 10;
   return (
-    <div className={styles.question}>
-      <div className={styles.questionText}>
-        <TickerText text={qq.question} showDot={true} />
-      </div>
-      {qq.answers.map((text, idx) => {
-        const q = (
-          <button
-            key={idx}
-            className={classnames({
-              [styles.answer]: true,
-              [styles.selectedAnswer]:
-                _humanAnswer === idx || _botAnswer === idx,
-            })}
-            onClick={() => {
-              setHumanAnswer(idx);
-            }}
-          >
-            <span>
-              <TickerText
-                text={indexToLetter(idx) + '. '}
-                delayBy={runningTotal}
-              />
-            </span>
-            <span>
-              <TickerText text={text} delayBy={runningTotal + 3} />
-            </span>
-            {_botAnswer === idx && <span className={styles.botAvatar}>A</span>}
-            {_humanAnswer === idx && (
-              <span className={styles.humanAvatar}>Y</span>
-            )}
-          </button>
-        );
+    <div className={styles.questionContainer}>
+      <div className={styles.question}>
+        <div className={styles.questionText}>
+          <TickerText text={qq.question} showDot={true} />
+        </div>
+        {qq.answers.map((text, idx) => {
+          const q = (
+            <button
+              key={idx}
+              className={classnames({
+                [styles.answer]: true,
+                [styles.selectedAnswer]:
+                  _humanAnswer === idx || _botAnswer === idx,
+              })}
+              onClick={() => {
+                setHumanAnswer(idx);
+              }}
+            >
+              <span>
+                <TickerText
+                  text={indexToLetter(idx) + '.'}
+                  delayBy={runningTotal}
+                />
+              </span>
+              &nbsp;
+              <span>
+                <TickerText text={text} delayBy={runningTotal + 3} />
+              </span>
+              {_botAnswer === idx && (
+                <span className={styles.botAvatar}>
+                  <Image
+                    src={'/bot.svg'}
+                    width={12}
+                    height={12}
+                    alt="Bot"
+                    title="Your AI teammate"
+                  />
+                </span>
+              )}
+              {_humanAnswer === idx && (
+                <span className={styles.humanAvatar}>
+                  <Image
+                    src={'/avatar.svg'}
+                    width={12}
+                    height={12}
+                    alt="You"
+                    title="You"
+                    className={styles.avatarImg}
+                  />
+                </span>
+              )}
+            </button>
+          );
 
-        runningTotal += text.length + 3;
-        return q;
-      })}
+          runningTotal += text.length + 3;
+          return q;
+        })}
+        {!humanAnswer &&
+          !botAnswer &&
+          _humanAnswer !== undefined &&
+          _botAnswer !== undefined && (
+            <button
+              className={styles.submit}
+              onClick={() => {
+                onSubmit(_humanAnswer, _botAnswer);
+              }}
+            >
+              Final answer?
+            </button>
+          )}
+      </div>
       <Thread threadId={qq.cordThreadID} />
-      {!humanAnswer && !botAnswer && !!_humanAnswer && !!_botAnswer && (
-        <button
-          onClick={() => {
-            onSubmit(_humanAnswer, _botAnswer);
-          }}
-        >
-          Final answer?
-        </button>
-      )}
     </div>
   );
 }
