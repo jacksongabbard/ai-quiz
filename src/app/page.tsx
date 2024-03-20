9be7f026601abb3db23d428eb3d4a39bb1b0ee39
Josh Watzman <jwatzman@cord.com>
2024-03-20 11:31:21 +0000

    Sketch question type and loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { QuizQuestion, questions } from "@/lib/questions";
 import styles from "./page.module.css";
 
-export default function Home() {
+type QuizData = {
+  cordAccessToken: string;
+  questions: (QuizQuestion & {
+    cordThreadID: string;
+  })[];
+};
+
+async function getQuizData(): Promise<QuizData> {
+  return {
+    cordAccessToken: "",
+    questions: questions.map((q) => ({ ...q, cordThreadID: "" })),
+  };
+}
+
+export default async function Home() {
+  const _ = await getQuizData();
   return <main className={styles.main}>Quiz coming soon!</main>;
 }
---
+++ src/lib/questions.ts
@@ -0,0 +1,13 @@
+export type QuizQuestion = {
+	question: string;
+	answers: string[];
+	correctAnswerIndex: number;
+};
+
+export const questions: QuizQuestion[] = [{
+	question: 'Which of these words, if you remove one letter, is an anagram of "China"?',
+	answers: [
+		'Chains', 'Chins', 'Panic', 'Change'
+	],
+	correctAnswerIndex: 0
+}];
