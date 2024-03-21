14b3987075a8d7f82e1c9bfd8160f690ebf8cb2c
Jackson Gabbard <jg@jg.gg>
2024-03-21 15:09:35 +0000

    Apprently viewport meta tag needs to go like this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,6 @@ export const metadata: Metadata = {
     shortcut: '/android-chrome-512x512.svg',
     apple: '/apple-touch-icon.png',
   },
-  viewport:
-    'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ import { BaseQuizQuestion } from '@/lib/questions';
 import { Quiz } from '@/ui/Quiz';
 
 import styles from './page.module.css';
+import Head from 'next/head';
 
 export type ClientQuizQuestion = BaseQuizQuestion & { cordThreadID: string };
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <div className="gradient"></div>
       <main className={styles.main}>
         <Quiz />
