16f978dee69f9673e208fc95cb24244cca2168fa
Jackson Gabbard <jg@jg.gg>
2024-03-22 13:33:01 +0000

    Add OG meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,27 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'AI Quiz Challenge',
+  title: 'AI Co-op Quiz',
   description: 'Can you and your AI buddy succeed together?',
   icons: {
     icon: '/favicon-32x32.png',
     shortcut: '/android-chrome-512x512.svg',
     apple: '/apple-touch-icon.png',
+  },
+  openGraph: {
+    title: 'AI Co-op Quiz',
+    description: 'Can you and your AI buddy succeed together?',
+    images: {
+      url: '/opengraph-image.png',
+    },
+    url: 'https://quiz.cord.com',
+  },
+  twitter: {
+    title: 'AI Co-op Quiz',
+    description: 'Can you and your AI buddy succeed together?',
+    images: {
+      url: '/opengraph-image.png',
+    },
   },
 };
 
