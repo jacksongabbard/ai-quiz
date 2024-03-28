5fbfa1ceb8cea01438442d538605936c5d2f9abf
Jackson Gabbard <jg@jg.gg>
2024-03-28 13:20:01 +0000

    Make the start-screen ad less aggro

--- src/ui/Start.tsx
+++ src/ui/Start.tsx
@@ -43,22 +43,25 @@ export function Start({
           </button>
         )}
       </div>
-      <div className={styles.poweredBy}>
-        Powered by the Cord SDK
-        <br />
-        for chat and message streaming
-        <br />
-        <a href="https://cord.com/" title="React chat UI library and backend">
-          <Image
-            className={styles.logo}
-            src="/cord-logo.svg"
-            width={180}
-            height={59}
-            alt={'Cord Logo'}
-            title={'Cord offers the most feature-rich chat SDK on the internet'}
-          />
-        </a>
-      </div>
+      <a
+        className={styles.poweredBy}
+        href="https://cord.com/"
+        title="React chat UI library and backend"
+      >
+        <Image
+          className={styles.logo}
+          src="/cord-logo.svg"
+          width={90}
+          height={30}
+          alt={'Cord Logo'}
+          title={'Cord offers the most feature-rich chat SDK on the internet'}
+        />
+        <span>
+          Powered by the Cord SDK
+          <br />
+          for chat and message streaming
+        </span>
+      </a>
     </div>
   );
 }
