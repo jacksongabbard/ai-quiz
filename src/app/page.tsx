9192fb0a81cff49d57b18bd40577cdf7e443a2f0
Jackson Gabbard <jg@jg.gg>
2024-06-17 14:46:42 +0100

    Nuke the quiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+'use client';
+
 import { BaseQuizQuestion } from '@/lib/questions';
-import { Quiz } from '@/ui/Quiz';
 
 import styles from './page.module.css';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export type ClientQuizQuestion = BaseQuizQuestion & { cordThreadID: string };
 
 export default function Home() {
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = 'https://cord.com/';
+    }, 3000);
+  }, []);
+
   return (
     <>
       <Head>
@@ -17,7 +25,7 @@ export default function Home() {
       </Head>
       <div className="gradient"></div>
       <main className={styles.main}>
-        <Quiz />
+        <a href="https://cord.com/">Redirecting to Cord.com...</a>
       </main>
     </>
   );
